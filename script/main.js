7bac81cfbcdb54169b4c1d277f5232be639c9842
brynathn <bryan.nathaniel73@gmail.com>
2024-08-12 02:32:36 +0700

    last

--- script/main.js
+++ script/main.js
@@ -3,11 +3,11 @@ function updateImage() {
   const width = window.innerWidth;
 
   if (width <= 320) {
-    image.src = "/img/mockup_sm.png";
+    image.src = "img/mockup_sm.png";
   } else if (width <= 568) {
-    image.src = "/img/mockup_md.png";
+    image.src = "img/mockup_md.png";
   } else if (width <= 744) {
-    image.src = "/img/mockup_lg.png";
+    image.src = "img/mockup_lg.png";
   }
 }
 window.addEventListener("load", updateImage);
