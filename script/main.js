2ff8459807bbcbfb3ee82365a3c44202b9feec24
brynathn <bryan.nathaniel73@gmail.com>
2024-08-11 15:52:03 +0700

    scnd

--- script/main.js
+++ script/main.js
@@ -12,3 +12,83 @@ function updateImage() {
 }
 window.addEventListener("load", updateImage);
 window.addEventListener("resize", updateImage);
+
+$(document).ready(function () {
+  var slider = $(".slider");
+  var cardWidth;
+
+  function updateCardWidth() {
+    cardWidth = $(".card").outerWidth(true);
+    updateMaxScroll();
+  }
+
+  function updateMaxScroll() {
+    slider.data("max-scroll", slider[0].scrollWidth - slider.outerWidth());
+  }
+
+  function scrollSlider(amount) {
+    var leftPos = slider.scrollLeft();
+    var maxScroll = slider.data("max-scroll");
+    if (amount > 0 && leftPos < maxScroll) {
+      slider.animate(
+        { scrollLeft: Math.min(leftPos + amount, maxScroll) },
+        300
+      );
+    } else if (amount < 0 && leftPos > 0) {
+      slider.animate({ scrollLeft: Math.max(leftPos + amount, 0) }, 300);
+    }
+  }
+
+  $(".next").click(function () {
+    scrollSlider(cardWidth);
+  });
+
+  $(".prev").click(function () {
+    scrollSlider(-cardWidth);
+  });
+
+  updateCardWidth();
+  updateMaxScroll();
+
+  $(window).resize(function () {
+    updateCardWidth();
+  });
+});
+
+
+
+$(document).ready(function () {
+  var slider = $(".slider");
+  var isDragging = false;
+  var startX;
+  var scrollLeft;
+
+  slider.on("mousedown", function (e) {
+    isDragging = true;
+    startX = e.pageX - slider.offset().left;
+    scrollLeft = slider.scrollLeft();
+    slider.css("cursor", "grabbing");
+    e.preventDefault();
+  });
+
+  $(window).on("mouseup", function () {
+    isDragging = false;
+    slider.css("cursor", "grab");
+  });
+
+  slider.on("mousemove", function (e) {
+    if (!isDragging) return;
+    e.preventDefault();
+    var x = e.pageX - slider.offset().left;
+    var walk = (x - startX);
+    slider.scrollLeft(scrollLeft - walk);
+  });
+
+  slider.on("mouseleave", function () {
+    isDragging = false;
+    slider.css("cursor", "grab");
+  });
+});
+
+
+
